d7728f29d3c2cccf36401231ec5ef3aa0c6b0dd3
Bigwhale001 <bigwhale86.gmail.com>
2025-08-30 10:25:17 +0100

    initial commit

---
+++ digital.js
@@ -0,0 +1,67 @@
+const body = document.querySelector("body");
+const backdrop = document.getElementById("backdrop");
+const header = document.getElementById("header");
+const hamburger = document.querySelector(".hamburger_menu");
+const mockUp = header.querySelector(".intro__mockup");
+const mobileNav = document.getElementById("mobile__nav");
+const btns = document.querySelectorAll(".request_invite");
+
+let open = false;
+
+// Functions
+function openNav() {
+  open = !open;
+  if (open) {
+    hamburger.innerHTML = `&#10006;`;
+  } else {
+    hamburger.innerHTML = `<img src="./images/icon-hamburger.svg" alt="menu-icon">`;
+  }
+
+  // toggle the hidden class
+  mockUp.classList.toggle("hidden");
+  backdrop.classList.toggle("hidden");
+  mobileNav.classList.toggle("hidden");
+}
+
+(function () {
+  const showMockUp = window.matchMedia("(min-width: 48rem)");
+  showMockUp.addEventListener("change", function (e) {
+    if (e.matches && open) mockUp.classList.remove("hidden");
+    else if (!e.matches && open) mockUp.classList.add("hidden");
+  });
+})();
+
+// event listeners
+hamburger.addEventListener("click", openNav);
+document.addEventListener("keyup", function (ev) {
+  if (ev.key === "Escape" && open) {
+    openNav();
+  }
+});
+
+btns.forEach((b) =>
+  b.addEventListener("click", function (e) {
+    e.preventDefault();
+  })
+);
+
+// section revealing
+const sections = document.querySelectorAll(".section");
+
+function showSection(entries) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("hide-section");
+  sectObserver.unobserve(entry.target);
+}
+const sectObserver = new IntersectionObserver(showSection, {
+  root: null,
+  threshold: 0.12,
+});
+
+sections.forEach(function (sect) {
+  sectObserver.observe(sect);
+  sect.classList.add("hide-section");
+});
